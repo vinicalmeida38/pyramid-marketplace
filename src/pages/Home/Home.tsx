372adab02c815fd6eb3068674153869c1081ba11
vinicalmeida38 <vinicalmeida38@gmail.com>
2021-08-12 23:39:23 -0300

    Create product component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,22 @@
 import React from "react";
+import "./Home.css";
+
 import Header from "../../components/Header/Header";
+import Product from "../../components/Product/Product";
+
+import productMocks from "../../assets/mocks/products.json";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <div className="product-grid">
+        {productMocks.map((mock) => {
+          return (
+            <Product name={mock.name} image={mock.image} price={mock.price} />
+          );
+        })}
+      </div>
     </>
   );
 };
