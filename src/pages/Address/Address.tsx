e53ecd22075db746015cfbc6156a96ab2c14708b
vinicalmeida38 <vinicalmeida38@gmail.com>
2021-08-22 21:46:35 -0300

    Form disabled button

--- src/pages/Address/Address.tsx
+++ src/pages/Address/Address.tsx
@@ -1,9 +1,19 @@
-import React from "react";
 import "./Address.css";
 
 import SimpleHeader from "../../components/Header/SimpleHeader";
+import { useState } from "react";
 
 const Address = () => {
+  const [name, setName] = useState("");
+  const [CEP, setCEP] = useState("");
+  const [state, setState] = useState("");
+  const [city, setCity] = useState("");
+  const [street, setStreet] = useState("");
+  const [district, setDistrict] = useState("");
+  const [number, setNumber] = useState("");
+  const [complement, setComplement] = useState("");
+  const [phone, setPhone] = useState("");
+
   return (
     <>
       <SimpleHeader />
@@ -19,6 +29,8 @@ const Address = () => {
             name="name"
             id="name"
             placeholder="Nome completo"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
           <input
             className="input-pyramid"
@@ -26,6 +38,8 @@ const Address = () => {
             name="cep"
             id="cep"
             placeholder="CEP"
+            value={CEP}
+            onChange={(e) => setCEP(e.target.value)}
           />
           <input
             className="input-pyramid"
@@ -33,6 +47,8 @@ const Address = () => {
             name="state"
             id="state"
             placeholder="Estado"
+            value={state}
+            onChange={(e) => setState(e.target.value)}
           />
           <input
             className="input-pyramid"
@@ -40,6 +56,8 @@ const Address = () => {
             name="city"
             id="city"
             placeholder="Cidade"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
           />
           <input
             className="input-pyramid"
@@ -47,6 +65,8 @@ const Address = () => {
             name="street"
             id="street"
             placeholder="Rua/Avenida"
+            value={street}
+            onChange={(e) => setStreet(e.target.value)}
           />
           <input
             className="input-pyramid"
@@ -54,6 +74,8 @@ const Address = () => {
             name="district"
             id="district"
             placeholder="Bairro"
+            value={district}
+            onChange={(e) => setDistrict(e.target.value)}
           />
           <input
             className="input-pyramid"
@@ -61,6 +83,8 @@ const Address = () => {
             name="number"
             id="number"
             placeholder="Número"
+            value={number}
+            onChange={(e) => setNumber(e.target.value)}
           />
           <input
             className="input-pyramid"
@@ -68,6 +92,8 @@ const Address = () => {
             name="complement"
             id="complement"
             placeholder="Complemento"
+            value={complement}
+            onChange={(e) => setComplement(e.target.value)}
           />
           <input
             className="input-pyramid"
@@ -75,11 +101,28 @@ const Address = () => {
             name="contact"
             id="contact"
             placeholder="Telefone"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
           />
 
           <div className="step-button">
             <hr />
-            <button className="button-pyramid" type="submit">
+            <button
+              className={
+                name === "" ||
+                CEP === "" ||
+                state === "" ||
+                city === "" ||
+                street === "" ||
+                district === "" ||
+                number === "" ||
+                complement === "" ||
+                phone === ""
+                  ? "button-pyramid disabled-button-pyramid"
+                  : "button-pyramid"
+              }
+              type="submit"
+            >
               Continuar
             </button>
           </div>
